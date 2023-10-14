e0138e03b6e98c0835a7fe2c1906fa5408052ec8
Carlos Iglesias <Magulon26@gmail.com>
2023-10-14 11:41:34 -0300

    Actividad 1

---
+++ Actividades_S21/src/Actividad1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Carlos
+ */
+public class Actividad1 {
+    public static void main(String[] args){
+        
+        //Generamos 2 atributos
+        int numero1 = 3;
+        int numero2 = 7;
+        
+        //Realizamos el calculo
+        int resultado = numero1 + numero2;
+            
+        //Mostramos el resultado por consola
+        
+        System.out.println("La suma de: " + numero1 + " y " + numero2 + " es " + resultado);
+        
+    }
+}
